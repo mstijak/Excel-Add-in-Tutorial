9c4327e6dc99c95cd69d3f46e654080537f126ff
Ricky Kirkham <rickki@microsoft.com>
2019-02-21 15:04:11 -0800

    fix issue 6

--- Start/app.js
+++ Start/app.js
@@ -14,9 +14,10 @@
             // TODO1: Determine if the user's version of Office supports all the
             //        Office.js APIs that are used in the tutorial.
 
-            // TODO2: Assign event handlers and other initializaton logic.
+            // TODO2: Assign event handlers and other initialization logic.
 
         });
+    });
 
     // TODO3: Add handlers and business logic functions here.
 
